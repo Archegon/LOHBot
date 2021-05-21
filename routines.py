08d1a37d86587e483495f438616df83f71560d01
Nircniv <ng.ta.wei@outlook.com>
2021-05-21 22:49:02 +0800

    Initial Commit

---
+++ routines.py
@@ -0,0 +1,13 @@
+from modules import Module, ModuleManager
+
+
+# Fairy Collector
+fairy = Module('Fairy Collector', 1800)
+
+
+def fairy_routine():
+    pass
+
+
+fairy.set_routine(fairy_routine())
+
