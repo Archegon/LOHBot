623714ba7231353a022623f90013ff33d6058ab0
Nircniv <ng.ta.wei@outlook.com>
2021-05-23 14:53:09 +0800

    Added timeout to login routine.

--- routines/auto_restart.py
+++ routines/auto_restart.py
@@ -1,6 +1,6 @@
 from game import game_control
 from modules import Module
-from routines import routines_setup
+from routines.routines_setup import module_manager
 
 # Auto Restart
 auto_restart = Module('Auto Restart', 3600, load_data=False)
@@ -8,7 +8,7 @@ auto_restart.set_cooldown_time()
 
 
 def auto_restart_routine():
-    routines_setup.module_manager.stop = True
+    module_manager.stop = True
     game_control.kill_program()
 
 
--- routines/login_routine.py
+++ routines/login_routine.py
@@ -2,6 +2,7 @@ import time
 from modules import Module
 from position import Region, Point
 from game import game_control
+from routines.routines_setup import module_manager
 
 # Login Module
 login = Module('Login', start_only=True)
@@ -11,12 +12,24 @@ def login_routine():
     touch_to_start = Region(1637, 920, 582, 720, 1057, 820)
     touch_b = Point(1637, 920, 616, 517)
 
+    start_time = time.time()
+    time_out = 300
+    kill_game = False
+
     while not game_control.at_page('Main'):
         if touch_to_start.check_for('touch to start'):
             touch_b.click()
 
-    time.sleep(5)
-    game_control.back_to_main()
+        if time.time() - start_time >= time_out:
+            kill_game = True
+            break
+
+    if not kill_game:
+        time.sleep(5)
+        game_control.back_to_main()
+    else:
+        module_manager.stop = True
+        game_control.kill_program()
 
 
 login.set_routine(login_routine)
